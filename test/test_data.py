64b54c0e061b03eb8943cce952c2ea2475ea6035
sei-vturri <90710001+sei-vturri@users.noreply.github.com>
2021-10-22 08:41:22 -0400

    Feature/jb 309 fix label names (#8)
    * New get_label_mapping first attempt
    
    * WIP testing
    
    * Added more info for unit testing
    
    * Eliminate circular import
    
    * Added in eval case and more tests
    
    * Tweaked eval case
    
    * More changes to unit tet and eval config handling
    
    * Improve comment specificity
    
    Improve the comments so that they specify which particular file is being checked
    
    * Refactored to use filesystem helper functions
    
    * Updated doc markings; updated a logger error message; adjusted some import statements
    
    * Modified function to prioritize output.json file, then overrid configs, then model manager info
    
    * Updated TrainingOutput class to include label_names
    
    * Finished adding in label_mapping field to training output
    
    * Modified functions to include Nick's updates (WIP)
    
    * Debugging based on unit testing
    
    * Switched to using retrieve_label_names for dataset configs and other small changes
    
    * Fixed a line that was too long
    
    Co-authored-by: Nick Winski <nwinski@sei.cmu.edu>

--- test/test_data.py
+++ test/test_data.py
@@ -1,46 +1,24 @@
 #! /usr/bin/env python3
 
 # ======================================================================================================================
-#  Copyright 2021 Carnegie Mellon University.
+# Juneberry - General Release
 #
-#  NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS"
-#  BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER
-#  INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED
-#  FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM
-#  FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+# Copyright 2021 Carnegie Mellon University.
 #
-#  Released under a BSD (SEI)-style license, please see license.txt or contact permission@sei.cmu.edu for full terms.
+# NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS"
+# BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER
+# INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED
+# FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM
+# FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
 #
-#  [DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution.
-#  Please see Copyright notice for non-US Government use and distribution.
+# Released under a BSD (SEI)-style license, please see license.txt or contact permission@sei.cmu.edu for full terms.
 #
-#  This Software includes and/or makes use of the following Third-Party Software subject to its own license:
+# [DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution.  Please see
+# Copyright notice for non-US Government use and distribution.
 #
-#  1. PyTorch (https://github.com/pytorch/pytorch/blob/master/LICENSE) Copyright 2016 facebook, inc..
-#  2. NumPY (https://github.com/numpy/numpy/blob/master/LICENSE.txt) Copyright 2020 Numpy developers.
-#  3. Matplotlib (https://matplotlib.org/3.1.1/users/license.html) Copyright 2013 Matplotlib Development Team.
-#  4. pillow (https://github.com/python-pillow/Pillow/blob/master/LICENSE) Copyright 2020 Alex Clark and contributors.
-#  5. SKlearn (https://github.com/scikit-learn/sklearn-docbuilder/blob/master/LICENSE) Copyright 2013 scikit-learn 
-#      developers.
-#  6. torchsummary (https://github.com/TylerYep/torch-summary/blob/master/LICENSE) Copyright 2020 Tyler Yep.
-#  7. pytest (https://docs.pytest.org/en/stable/license.html) Copyright 2020 Holger Krekel and others.
-#  8. pylint (https://github.com/PyCQA/pylint/blob/main/LICENSE) Copyright 1991 Free Software Foundation, Inc..
-#  9. Python (https://docs.python.org/3/license.html#psf-license) Copyright 2001 python software foundation.
-#  10. doit (https://github.com/pydoit/doit/blob/master/LICENSE) Copyright 2014 Eduardo Naufel Schettino.
-#  11. tensorboard (https://github.com/tensorflow/tensorboard/blob/master/LICENSE) Copyright 2017 The TensorFlow 
-#                  Authors.
-#  12. pandas (https://github.com/pandas-dev/pandas/blob/master/LICENSE) Copyright 2011 AQR Capital Management, LLC,
-#             Lambda Foundry, Inc. and PyData Development Team.
-#  13. pycocotools (https://github.com/cocodataset/cocoapi/blob/master/license.txt) Copyright 2014 Piotr Dollar and
-#                  Tsung-Yi Lin.
-#  14. brambox (https://gitlab.com/EAVISE/brambox/-/blob/master/LICENSE) Copyright 2017 EAVISE.
-#  15. pyyaml  (https://github.com/yaml/pyyaml/blob/master/LICENSE) Copyright 2017 Ingy döt Net ; Kirill Simonov.
-#  16. natsort (https://github.com/SethMMorton/natsort/blob/master/LICENSE) Copyright 2020 Seth M. Morton.
-#  17. prodict  (https://github.com/ramazanpolat/prodict/blob/master/LICENSE.txt) Copyright 2018 Ramazan Polat
-#               (ramazanpolat@gmail.com).
-#  18. jsonschema (https://github.com/Julian/jsonschema/blob/main/COPYING) Copyright 2013 Julian Berman.
+# This Software includes and/or makes use of Third-Party Software subject to its own license.
 #
-#  DM21-0689
+# DM21-0884
 #
 # ======================================================================================================================
 
@@ -56,6 +34,8 @@ from juneberry.config.model import ModelConfig
 import juneberry.data as jb_data
 from juneberry.lab import Lab
 from juneberry.transform_manager import TransformManager
+from juneberry.filesystem import ModelManager
+from juneberry.config.training_output import TrainingOutputBuilder
 
 from test_coco_utils import make_sample_coco
 import test_model_config
@@ -755,3 +735,34 @@ def mock_load_tabular_data(mc, ds):
         return ['tab_train'], ['tab_val']
     else:
         return ['tab_train'], []
+
+
+def test_get_label_mapping():
+    # Binary sample files
+    model_name = "tabular_binary_sample"
+    model_manager = ModelManager(model_name)
+    model_config = model_manager.get_model_config()
+    train_config = "models/tabular_binary_sample/train_data_config.json"
+    test_labels = {0: "outer", 1: "inner"}
+
+    # Unit tests
+    test_source = "training dataset config via model config via model manager"
+    func_labels, func_source = jb_data.get_label_mapping(model_manager=model_manager, show_source=True)
+    assert test_labels == func_labels
+    assert test_source == func_source
+
+    # TODO: write a unit test for the training output case
+
+    func_labels = jb_data.get_label_mapping(model_config=model_config, show_source=True)
+    assert func_labels is None
+
+    test_source = "training dataset config"
+    func_labels, func_source = jb_data.get_label_mapping(train_config=train_config, show_source=True)
+    assert test_labels == func_labels
+    assert test_source == func_source
+
+    test_source = "training dataset config"
+    func_labels, func_source = jb_data.get_label_mapping(model_config=model_config, train_config=train_config,
+                                                         show_source=True)
+    assert test_labels == func_labels
+    assert test_source == func_source
