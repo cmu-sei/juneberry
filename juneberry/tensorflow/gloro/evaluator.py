c9bae998e2bd5a2cb4bacc78456466e6bf24aced
sei-jwhelland <jwhelland@cert.org>
2021-10-25 13:36:44 -0400

    Tensorflow implementation of model inversion and prototype evaluation of model that computes the inversions.

--- juneberry/tensorflow/gloro/evaluator.py
+++ juneberry/tensorflow/gloro/evaluator.py
@@ -34,6 +34,194 @@ import juneberry.tensorflow.evaluator
 logger = logging.getLogger(__name__)
 
 
+# TODO: move these util functions to workspace
+from einops import rearrange
+from typing import Optional, Union
+
+try:
+    import torch
+    HAS_TORCH = True
+except ImportError:
+    HAS_TORCH = False
+try:
+    import tensorflow as tf
+    HAS_TF = True
+except ImportError:
+    HAS_TF = False
+
+if not HAS_TORCH and not HAS_TF:
+    raise EnvironmentError('Must have either pytorch or tensorflow installation.')
+
+if HAS_TORCH and HAS_TF:
+    Tensor = Union[torch.Tensor, tf.Tensor]
+else:
+    Tensor = torch.Tensor if HAS_TORCH else tf.Tensor
+
+def unsqueeze(
+    x        :Tensor, 
+    n        :int    =1, 
+    prepend  :bool   =False
+) -> Tensor:
+    """Prepend or append n-many singleton dimensions to a tensor.
+    """
+    if prepend:
+        return rearrange(x, '... ->' + ' 1' * n + ' ...')
+    else:
+        return rearrange(x, '... -> ...' + ' 1' * n)
+        
+
+def unsqueeze_like(
+    x        :Tensor, 
+    y        :Tensor,
+    dim      :Optional[int]  =None,
+    prepend  :bool           =False,
+) -> Tensor:
+    """Prepend and append singleton dimensions to `x` to match the shape of `y`. 
+    `y` must have at least as many dimensions as `x`.
+
+    Example usage: 
+    ```
+    x = torch.randn(3)
+    y = torch.randn(3,3,32,32)
+    unsqueeze_like(x, y)         # returns shape (3,1,1,1)
+    unsqueeze_like(x, y, dim=1)  # returns shape (3,1,1)
+    unsqueeze_like(x, y, dim=2)  # error
+    ```
+
+    :param x        : Tensor to reshape.
+    :param y        : Tensor to match.
+    :param dim      : Specifies a dimension of `y` to match. If `None`, then automatically. 
+    :param prepend  : `False` only appends singleton dimensions.
+                      `True` prepends singleton dimensions as well.
+                      NOTE: broadcasting semantics don't require prepend singleton dimensions.
+    """
+    assert len(x.shape) <= len(y.shape), \
+        f'Argument `x` of shape `{x.shape}` has more dimensions than argument `y` of shape `{y.shape}`.'\
+        'Did you mean to match the dimensions of `y` to `x` instead?'
+    if x.shape == y.shape:
+        return x
+    if len(x.shape) == 0:
+        x = x[None]
+
+    # Try to find the first `dim_x` dimension of `x` that is non-singleton.
+    for dim_x in range(len(x.shape)):
+        if x.shape[dim_x] != 1:
+            break
+
+    # Find the `dim_y` dimension of `y` matching the `dim_x` dimension of `x` so we can calculate the number of 
+    # singleton dimensions to prepend and append.
+    if dim is not None:
+        dim_y = dim
+        assert x.shape[dim_x] == y.shape[dim_y], \
+            f'Dimensions do not match: `x.shape[{dim_x}] != y.shape[{dim_y}]` '\
+            f'with values `{x.shape[dim_x]} != {y.shape[dim_y]}`'
+    else:
+        matched = False
+        for dim_y in range(len(y.shape)):
+            if ((x.shape[dim_x] == 1) or              # dimension size 1 can match any via broadcasting
+                (y.shape[dim_y] == x.shape[dim_x])):  
+                matched = True
+                break 
+        assert matched, \
+            f'Could not find a dimension in `y.shape == {y.shape}` matching `x.shape[{dim_x}] == {x.shape[dim_x]}`'
+
+    # Compute the number of prepend and append singleton dimensions
+    n_append = max(0, len(y.shape[dim_y:]) - len(x.shape[dim_x:]))
+    n_prepend = max(0, dim_y - dim_x)
+    n_extra_dims = len(y.shape) - len(x.shape) - n_prepend - n_append
+    assert n_extra_dims == 0, \
+        f'Matched dimension `x.shape[{dim_x}] == {x.shape[dim_x]}` to `y.shape[{dim_y}] == {y.shape[dim_y]}`, '\
+        f'but this results in {n_prepend} prepend and {n_append} append dimensions, resulting in '\
+        f'{n_extra_dims} too many singleton dimensions. The shapes are `x.shape == {x.shape}` and '\
+        f'`y.shape == {y.shape}`'
+
+    # broadcasting semantics no longer require singleton dimensions prepended
+    # return unsqueeze(unsqueeze(x, n_prepend, prepend=True), n_append)
+    x_new = unsqueeze(x, n_append)
+    if prepend:
+        x_new = unsqueeze(x_new, n_prepend, prepend=True)
+    return x_new 
+
+
+# TODO: move this to a workspace file
+from copy import deepcopy
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
+import einops
+
+
+def normalize(x, geometry, *, eps=1e-10) -> tf.Tensor:
+    """A collection of batched normalization functions.
+    """
+    # l2 normalization over all axes > 0 flattened. Can also be thought of as Frobenius normalization.
+    if geometry == 'l2':
+        x_flat = einops.rearrange(x, 'b ... -> b (...)') 
+        norm = tf.linalg.norm(x_flat, axis=1)
+        return x / (unsqueeze_like(norm, x) + eps)
+
+    # Normalizing wrt to linf norm is equivalent to projecting onto boundary of the hypercube [-1,1]^d.
+    # The projection operator is therefore elementwise sign.
+    elif geometry == 'linf':
+        return tf.sign(x)
+
+    # Normalize so that each tensor in the batch has pixel values in the range [0,1].
+    elif geometry == '01':
+        x_flat = einops.rearrange(x, 'b ... -> b (...)')
+        max_val = tf.reduce_max(x_flat, axis=1)
+        min_val = tf.reduce_min(x_flat, axis=1)
+        return (x - unsqueeze_like(min_val, x)) / (unsqueeze_like(max_val, x) + eps)
+
+    else:
+        raise NotImplementedError(geometry)
+
+
+def _inversion_sgd(
+    model: keras.Model, 
+    x: tf.Tensor, 
+    target: tf.Tensor,
+    *,
+    step_size: float = 1e-3,
+    num_steps: int = 128,
+    geometry: str = 'l2',
+    weights_std: Optional[float] = None,
+) -> tf.Tensor:
+    """Perform `num_steps` many iterations of gradient ascent on the loss function with respect to the input.
+    Gradients are normalized according to a specified geometry to improve convergence.
+    In the case of `geometry='linf'`, this is equivalent to multiple iterates of FGSM targeting a specific label.
+
+    NOTE: This method is not meant for direct use. Instead, use `iterated_inversion`.
+    """
+    if weights_std is not None:
+        weights_orig = model.get_weights()
+        weights_noisy = deepcopy(weights_orig)
+        for layer in weights_noisy:
+            layer += weights_std * np.random.normal(size=layer.shape)
+        model.set_weights(weights_noisy)
+
+    for _ in range(num_steps):
+        with tf.GradientTape() as tape:
+            tape.watch(x)
+            prediction = model(x)
+            loss = model.loss(target, prediction)
+        
+        grad = tape.gradient(loss, x)
+        grad = normalize(grad, geometry)
+        x += step_size * grad
+
+    if weights_std is not None:
+        model.set_weights(weights_orig)
+
+    return x
+
+
+def iterated_inversion(model, x0, target_sequence, **inversion_kwargs):
+    x = tf.identity(x0)
+    for target in target_sequence:
+        x = _inversion_sgd(model, x, target, **inversion_kwargs)
+    return normalize(x, '01')
+
+
 # HACK - This needs to go into the workspace
 class Evaluator(juneberry.tensorflow.evaluator.Evaluator):
     def __init__(self, model_config: ModelConfig, lab, dataset: DatasetConfig, model_manager: ModelManager,
@@ -51,9 +239,35 @@ class Evaluator(juneberry.tensorflow.evaluator.Evaluator):
         # lib-gloro seems to save the model in a way that is extricated from metrics, losses, and optimizers.
         # As such, need to re-compile the model with the desired evaluation functions.
         # TODO: This should obviously be coming from a config file somewhere, but I'm not sure how to go about that yet.
-        import tensorflow.keras as keras
+        loss = keras.losses.SparseCategoricalCrossentropy()
+        metrics = [keras.metrics.SparseCategoricalAccuracy()]
+        logger.info(f'Compiling model with:'
+                    f'\n\tloss: {loss.__class__.__name__}'
+                    f'\n\tmetrics: {[m.__class__.__name__ for m in metrics]}')
         self.model.compile(
-            loss=keras.losses.SparseCategoricalCrossentropy(),
-            metrics=[keras.metrics.SparseCategoricalAccuracy()])
+            loss=loss,
+            metrics=metrics)
+        self.model.trainable = False
 
         logger.info("...complete")
+
+    def obtain_dataset(self) -> None:
+        """TODO: Get watermark metadata for this model.
+        """
+        # logger.info(f'{self.__class__.__name__} does not load ')
+        pass
+
+    def evaluate_data(self) -> None:
+        logger.info('Computing model inversions...')
+        input_shape = (64, *self.model.input_shape[1:])
+        x0 = tf.zeros(input_shape)
+        output_shape = self.model(x0).shape
+        target_sequence = [
+            np.random.randint(0, output_shape[-1], size=(len(x0),))]
+        x = iterated_inversion(
+            self.model, x0, target_sequence, 
+            step_size=1e-3, 
+            num_steps=128)
+        print('\n\n')
+        print(x.shape)
+        print('\n\n')
