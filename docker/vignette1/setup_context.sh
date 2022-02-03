36ac698b49a4a9a3d7fe61de1c2b0b782a08ce8f
Bill Shaw <wpshaw@sei.cmu.edu>
2022-02-03 15:10:21 -0500

    fix bugs, copy cifar data to context dir

--- docker/vignette1/setup_context.sh
+++ docker/vignette1/setup_context.sh
@@ -8,15 +8,21 @@ fi
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Make all the necessary dirs
-[ ! -d ${1} ] && mdkir -p "${1}"
-[ ! -d ${1}/dataroot ] && mdkir -p "${1}/dataroot"
+[ ! -d ${1} ] && mkdir -p "${1}"
+[ ! -d ${1}/dataroot ] && mkdir -p "${1}/dataroot"
 
 # Copy all the files we need, if not there
 cp ${SCRIPT_DIR}/juneberry.ini ${1}/.
 
-# If the cifar data isn't there
+# If the cifar data isn't in the context directory
+# and has already been downloaded into the dataroot directory
+DOWNLOADED_CIFAR_DIR=${SCRIPT_DIR}/dataroot/cifar-10-batches-py
+if [ -d ${DOWNLOADED_CIFAR_DIR} ]; then
+  rsync -hvrPt ${DOWNLOADED_CIFAR_DIR} ${1}
+fi
 
 # Clone Juneberry
 if [ ! -d ${1}/juneberry ]; then
   git clone --depth 1 https://github.com/cmu-sei/juneberry.git ${1}/juneberry
 fi
+
