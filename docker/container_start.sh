b43ec2cb06698b04d30841702a79a1ffeca38c26
Andrew O. Mellinger <aomellinger@sei.cmu.edu>
2022-04-14 10:13:28 -0400

    Fixed set user order.

--- docker/container_start.sh
+++ docker/container_start.sh
@@ -7,14 +7,15 @@ pip install -e /juneberry
 # Add in the bash completion
 source /juneberry/scripts/juneberry_completion.sh
 
+# Install any workspace code
+if [ -e "./setup.py" ]; then
+    echo "Installing workspace..."
+    pip install -e .
+fi
+
 # For non Darwin platforms setup the user
 # OSX Docker desktop already does this right
 if [ "${HOST_UNAME}" != "Darwin" ]; then
   /juneberry/docker/set_user.sh
 fi
 
-# Install any worksapc code
-if [ -e "./setup.py" ]; then
-    echo "Installing workspace..."
-    pip install -e .
-fi
