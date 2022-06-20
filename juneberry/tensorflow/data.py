3b8625a996fcae232774e5584196d7bcba232fd4
Andrew O. Mellinger <aomellinger@sei.cmu.edu>
2022-06-20 08:28:52 -0400

    The tensorflow function now passes in the label.

--- juneberry/tensorflow/data.py
+++ juneberry/tensorflow/data.py
@@ -147,7 +147,7 @@ def make_transform_manager(model_cfg: ModelConfig, ds_cfg: DatasetConfig, set_si
                                           TFTorchStagedTransform, eval)
 
 
-def _call_transforms_numpy(np_image, transforms):
+def _call_transforms_numpy(np_image, y, transforms):
     # Convert to image, apply transforms, and convert back.
     # print(f"+++++++++ type={type(np_image)} shape={np_image.shape}")
     # PIL doesn't like grayscale things to have 3 dimensions, so we have
@@ -156,17 +156,25 @@ def _call_transforms_numpy(np_image, transforms):
     if shape[2] == 1:
         np_image = np_image.reshape(shape[0], shape[1])
     img = Image.fromarray(np_image)
-    img = transforms(img)
+    img, new_label = transforms(img, label=y)
     if shape[2] == 1:
-        return np.array(img).reshape(shape[0], shape[1], 1)
-    return np.array(img)
+        return np.array(img).reshape(shape[0], shape[1], 1), new_label
+    return np.array(img), np.array(new_label)
 
 
 # TODO: Should this be a tf.function?
-def _transform_magic(image, transforms):
+def _transform_magic(image, y, transforms):
     # Set up a numpy function wrapper and then call it.
+    # Based on the explanation of py_function we need to explicitly set the shape to the
+    # graph code can figure out the size
+    # https://www.tensorflow.org/guide/data#applying_arbitrary_python_logic
+    # So, get the shape run the function and set it back.
+    im_shape = image.shape
     partial_func = partial(_call_transforms_numpy, transforms=transforms)
-    return tf.numpy_function(partial_func, [image], Tout=tf.uint8)
+    new_image, new_y = tf.numpy_function(partial_func, [image, y], Tout=[tf.uint8, tf.int64])
+    new_image.set_shape(im_shape)
+    new_y.set_shape(y.shape)
+    return new_image, new_y
 
 
 def _add_transforms_and_batching(dataset, model_cfg, ds_cfg, batch_size, eval):
@@ -188,7 +196,7 @@ def _add_transforms_and_batching(dataset, model_cfg, ds_cfg, batch_size, eval):
         None, eval)
 
     if transforms is not None:
-        dataset = dataset.map(lambda x, y: (_transform_magic(x, transforms), y))
+        dataset = dataset.map(lambda x, y: _transform_magic(x, y, transforms))
 
     # Apply batching
     return dataset.batch(batch_size)
