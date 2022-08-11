bb87925928409cc1e55d17b437e8c4e5e3e7d74f
sei-nwinski <90279549+sei-nwinski@users.noreply.github.com>
2022-08-11 13:42:16 -0400

    Jb 453 tuning unit tests (#119)
    * Wrote unit tests for Sprouts
    
    * Added tuning unit tests; added a dryrun mode to the tuner; small change to TuningSprout tests
    
    * Created unit tests for the TrainerFactory; fixed an issue in the unit test utils; added a table of contents to the report config spec
    
    * Fixed a workspace directory issue in the TrainerFactory tests; corrected some typos
    
    * Updated a method name that had a copy/paste error

---
+++ test/test_trainer_factory.py
@@ -0,0 +1,152 @@
+#! /usr/bin/env python3
+
+# ======================================================================================================================
+# Juneberry - General Release
+#
+# Copyright 2021 Carnegie Mellon University.
+#
+# NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS"
+# BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER
+# INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED
+# FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM
+# FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+#
+# Released under a BSD (SEI)-style license, please see license.txt or contact permission@sei.cmu.edu for full terms.
+#
+# [DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution.  Please see
+# Copyright notice for non-US Government use and distribution.
+#
+# This Software includes and/or makes use of Third-Party Software subject to its own license.
+#
+# DM21-0884
+#
+# ======================================================================================================================
+from unittest import TestCase
+
+import pytest
+
+from juneberry.config.dataset import DatasetConfig
+from juneberry.config.model import ModelConfig
+from juneberry.detectron2.trainer import Detectron2Trainer
+from juneberry.filesystem import ModelManager
+from juneberry.lab import Lab
+from juneberry.training.trainer_factory import TrainerFactory
+import utils
+
+
+class TestTrainerFactory(TestCase):
+    """
+    This group of tests exercise various aspects of the TrainerFactory.
+    """
+
+    @pytest.fixture(autouse=True)
+    def init_fixtures(self, tmp_path, caplog):
+        """
+        The purpose of this method is to make certain fixtures available inside of
+        the unittest.TestCase.
+        """
+        self.tmp_path = tmp_path
+        self.caplog = caplog
+
+        # Initialize a TrainerFactory.
+        self.trainer_factory = TrainerFactory()
+
+    def test_trainer_factory_attribute_init(self):
+        """
+        The purpose of this test is to verify that the TrainerFactory attributes are initialized properly.
+        """
+        # Confirm that all TrainerFactory attributes are set to None.
+        assert self.trainer_factory.dataset_config is None
+        assert self.trainer_factory.lab is None
+        assert self.trainer_factory.log_level is None
+        assert self.trainer_factory.model_config is None
+        assert self.trainer_factory.model_manager is None
+
+    def test_trainer_factory_set_model_config(self):
+        """
+        The purpose of this test is to exercise the 'set_model_config' method of the TrainerFactory under
+        various conditions.
+        :return:
+        """
+        # Verify that the TrainerFactory's model_config attribute isn't set.
+        assert self.trainer_factory.model_config is None
+
+        # Attempt to set the model_config attribute. It should fail because currently
+        # there is no ModelManager associated with the TrainerFactory.
+        self.trainer_factory.set_model_config()
+        assert "no ModelManager associated with the TrainerFactory" in self.caplog.text
+
+        # Create a temporary workspace in order to exercise the TrainerFactory's ability to load a
+        # ModelConfig using the ModelManager.
+        with utils.set_directory(self.tmp_path):
+            utils.setup_test_workspace(self.tmp_path)
+            utils.make_dt2_workspace(self.tmp_path)
+
+            # Create a ModelManager and associate it with the TrainerFactory and attempt to
+            # set the model config again.
+            self.trainer_factory.model_manager = ModelManager('text_detect', 'dt2/ut')
+            self.trainer_factory.set_model_config()
+
+            # Verify that the TrainerFactory's model_config attribute has been set and that the
+            # model_config's attributes match expectations.
+            assert self.trainer_factory.model_config is not None
+            assert self.trainer_factory.model_config.model_architecture.module == "COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml"
+            assert self.trainer_factory.model_config.epochs == 1
+            assert self.trainer_factory.model_config.validation.arguments.seed == 3554237221
+
+            # Create a variation of the previous ModelConfig and make slight adjustments to some of
+            # its attributes.
+            model_config = ModelConfig.load(self.trainer_factory.model_manager.get_model_config())
+            model_config.model_architecture.module = "new_value"
+            model_config.epochs = 10
+            model_config.validation.arguments.seed = 1234
+
+            # Attempt to set the TrainerFactory's model_config attribute to the ModelConfig variation
+            # that was just created.
+            self.trainer_factory.set_model_config(model_config=model_config)
+
+            # Confirm the model_config associated with the TrainerFactory reflects the previously
+            # adjusted values.
+            assert self.trainer_factory.model_config is not None
+            assert self.trainer_factory.model_config.model_architecture.module == "new_value"
+            assert self.trainer_factory.model_config.epochs == 10
+            assert self.trainer_factory.model_config.validation.arguments.seed == 1234
+
+    def test_trainer_factory_get_trainer(self):
+        """
+        The purpose of this test is to exercise the TrainerFactory's ability to produce Trainer objects.
+        """
+        # Verify that the TrainerFactory is unable to produce a Trainer when there is no model
+        # config associated with the TrainerFactory
+        assert self.trainer_factory.model_config is None
+        trainer = self.trainer_factory.get_trainer()
+        assert trainer is None
+
+        # Create a temporary workspace so that the file loading operations will work.
+        with utils.set_directory(self.tmp_path):
+            utils.setup_test_workspace(self.tmp_path)
+            utils.make_dt2_workspace(self.tmp_path)
+
+            # Now set the TrainerFactory attributes which are used to produce Trainers.
+            self.trainer_factory.lab = Lab(workspace=self.tmp_path, data_root=self.tmp_path)
+            self.trainer_factory.model_manager = ModelManager('text_detect', 'dt2/ut')
+            self.trainer_factory.model_config = ModelConfig.load(self.trainer_factory.model_manager.get_model_config())
+            self.trainer_factory.dataset_config = DatasetConfig.load("data_sets/text_detect_val.json")
+
+            # Get a Trainer from the TrainerFactory and verify some of its properties.
+            trainer = self.trainer_factory.get_trainer()
+            assert type(trainer) == Detectron2Trainer
+            assert trainer.resume is False
+            assert trainer.native is True
+            assert trainer.onnx is False
+
+            # Get another Trainer from the TrainerFactory, only this time set the parameters of the
+            # 'get_trainer' method to the opposite of their default values. Verify the type of the
+            # resulting Trainer, and confirm the three Trainer attributes were set correctly.
+            trainer = self.trainer_factory.get_trainer(resume=True, native=False, onnx=True)
+            assert type(trainer) == Detectron2Trainer
+            assert trainer.resume is True
+            assert trainer.native is False
+            assert trainer.onnx is True
+
+
