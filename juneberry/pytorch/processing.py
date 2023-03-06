0c09f0af94459e1219dede96b93dae78de0959f5
hmmoore14 <hmmoore@sei.cmu.edu>
2023-03-06 13:35:44 -0500

    Removing torch.cuda.current_device(), just using device variable, fixes error when using cpudev

--- juneberry/pytorch/processing.py
+++ juneberry/pytorch/processing.py
@@ -131,7 +131,7 @@ def setup_cuda_device(num_gpus, gpu):
         device = torch.device("cuda:" + str(gpu))
         torch.cuda.set_device(device)
         # Adding LOCAL_RANK env to support pytorch 22.11 upgrade
-        os.environ['LOCAL_RANK'] = str(torch.cuda.current_device())
+        os.environ['LOCAL_RANK'] = str(device)
 
     return device
 
